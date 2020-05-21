62acc76dc489ccb657b9ebebe77142385f5a98dc
Daniel Niasoff <4070285+dniasoff@users.noreply.github.com>
2020-05-22 00:16:31 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,11 @@ useradd -m github
 usermod -aG sudo github
 echo "%sudo ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
+mv ./entrypoint.sh /home/github/entrypoint.sh
+cd /home/github
+
 curl -Ls https://github.com/actions/runner/releases/download/v${GITHUB_RUNNER_VERSION}/actions-runner-linux-x64-${GITHUB_RUNNER_VERSION}.tar.gz | tar xz && sudo ./bin/installdependencies.sh
 
-chown github:github ./entrypoint.sh
+chown -R github:github /home/github/
 chmod u+x ./entrypoint.sh
-mv ./entrypoint.sh /home/github/entrypoint.sh
+
